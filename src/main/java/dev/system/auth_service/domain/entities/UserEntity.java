39d6c38214b3398e6cda3b713c6930f64b37cfd2
mtab-dev <abranteselcio@gmail.com>
2025-08-03 14:04:18 -0300

    feat: user entity (basic)

---
+++ src/main/java/dev/system/auth_service/domain/entities/UserEntity.java
@@ -0,0 +1,34 @@
+package dev.system.auth_service.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private String role;
+
+    @Column
+    private boolean active;
+}
