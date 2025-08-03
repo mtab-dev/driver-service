d59d5c78d7eb741ec95df130525930d8701a0cc9
mtab-dev <abranteselcio@gmail.com>
2025-08-03 20:39:19 -0300

    chore: dotenv config and postgres connection

--- src/main/java/dev/system/auth_service/AuthServiceApplication.java
+++ src/main/java/dev/system/auth_service/AuthServiceApplication.java
@@ -1,5 +1,6 @@
 package dev.system.auth_service;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv  = Dotenv.configure().load();
+
+		String port = dotenv.get("PORT", "8080");
+		System.setProperty("server.port", port);
+
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
