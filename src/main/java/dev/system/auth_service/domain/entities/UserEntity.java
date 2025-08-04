d5cf49c74e12e8cf6d7252f8c5e0fb9a2cd67e01
mtab-dev <abranteselcio@gmail.com>
2025-08-03 21:08:00 -0300

    feat: role enum

--- src/main/java/dev/system/auth_service/domain/entities/UserEntity.java
+++ src/main/java/dev/system/auth_service/domain/entities/UserEntity.java
@@ -1,24 +1,33 @@
 package dev.system.auth_service.domain.entities;
 
+import dev.system.auth_service.domain.enums.RoleEnum;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@Entity
+@Entity(name = "users")
 @Table(name = "users")
-public class UserEntity {
+public class UserEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @Column(unique = true, nullable = false)
     private String username;
+
+    @Column(nullable = false)
+    private String name;
 
     @Column(unique = true, nullable = false)
     private String email;
@@ -27,8 +36,26 @@ public class UserEntity {
     private String password;
 
     @Column
-    private String role;
+    private RoleEnum role;
 
     @Column
     private boolean active;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.role == RoleEnum.ADMIN) {
+            return  List.of(
+                    new SimpleGrantedAuthority("ROLE_ADMIN"),
+                    new SimpleGrantedAuthority("ROLE_USER"),
+                    new SimpleGrantedAuthority("ROLE_CLIENT"));
+        }
+        if(this.role == RoleEnum.USER) {
+            return  List.of(
+                    new SimpleGrantedAuthority("ROLE_USER"),
+                    new SimpleGrantedAuthority("ROLE_CLIENT")
+            );
+        }
+        return List.of(new SimpleGrantedAuthority("ROLE_CLIENT"));
+    }
+
 }
