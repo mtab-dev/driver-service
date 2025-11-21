773fb72b2da49037ad0f7c91510f924b044aa094
matheus <96802002+mtab-dev@users.noreply.github.com>
2025-11-20 23:48:59 -0300

    chore: allow all origins

--- src/main/java/dev/system/auth_service/infra/security/WebConfig.java
+++ src/main/java/dev/system/auth_service/infra/security/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://portal-client-demo.vercel.app/")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
